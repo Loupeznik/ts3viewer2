7180a8917992aa05b67b8a13d618894aa7d3c39f
Dominik Zarsky <dominik.zarsky@gmail.com>
2022-06-05 12:01:40 +0200

    finish connect to server functionality

--- src/DZarsky.TS3Viewer2.Web/src/components/forms/Input.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/components/forms/Input.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { ChangeEventHandler } from 'react';
 
 type InputProps = {
     id: string,
     value?: string,
     isDisabled?: boolean,
-    type: string
+    type: string,
+    onChange?: ChangeEventHandler<HTMLInputElement>
 }
 
-
-export const Input = ({ id, type, value = "", isDisabled = false }: InputProps) => {
+export const Input = ({ id, type, value = "", isDisabled = false, onChange }: InputProps) => {
     if (isDisabled)
     {
         return (
@@ -21,7 +21,6 @@ export const Input = ({ id, type, value = "", isDisabled = false }: InputProps) 
     return (
         <input type={ type } id={ id } className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg text-center
         bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 
-        dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={ value } />
+        dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={ value } onChange={onChange}  />
     )
-
 }
--- src/DZarsky.TS3Viewer2.Web/src/pages/Connect.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/pages/Connect.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from '../components/forms/Input';
 import { Label } from '../components/forms/Label';
 import { SubmitButton } from '../components/forms/SubmitButton';
 
 export const ConnectPage = () => {
+    const serverAddress = "127.0.0.1:9987"
+
+    const [username, setUsername] = useState({})
+
+    const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value)
+    }
+
+    const handleSubmit = (event: React.SyntheticEvent) => {
+        event.preventDefault()
+
+        window.location.assign("ts3server://" + serverAddress + "?nickname=" + username)
+    }
+
     return (
         <div>
             <div
@@ -11,18 +25,18 @@ export const ConnectPage = () => {
                 <h1 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Connect to the server</h1>
                 <div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                     <div
-                       className="w-full mt-3 md:w-1/4 bg-gray-800 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700">
-                        <form>
-                            <div className="flex flex-col gap-4">
+                        className="w-full mt-3 md:w-1/4 bg-gray-800 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700">
+                        <div className="flex flex-col gap-4">
+                            <form onSubmit={handleSubmit}>
                                 <div className="mb-4">
                                     <Label forElement="server" value="Server" />
-                                    <Input type="text" id="server" value="127.0.0.1:9987" isDisabled={ true } />
+                                    <Input type="text" id="server" value={serverAddress} isDisabled={true} />
                                     <Label forElement="name" value="Username" />
-                                    <Input type="text" id="name" />
+                                    <Input type="text" id="name" onChange={onChangeInput} />
                                 </div>
                                 <SubmitButton value="Connect" />
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
