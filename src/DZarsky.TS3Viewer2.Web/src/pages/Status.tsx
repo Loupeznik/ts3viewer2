e5cc0e68d42097e34bd4e823f7b76c05c9ac9383
Dominik Zarsky <dzarsky@dzarsky.eu>
2022-06-15 19:23:06 +0200

    optimize status page requests

--- src/DZarsky.TS3Viewer2.Web/src/pages/Status.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/pages/Status.tsx
@@ -4,18 +4,13 @@ import { ChannelDto, ChannelService, ClientDto, ClientService, ServerInfoDto, Se
 import { ClientList } from '../components/ClientList';
 
 export const StatusPage = () => {
+    const refreshInterval = 5000
     const [clients, setClients] = useState<ClientDto[]>([])
     const [channels, setChannels] = useState<ChannelDto[]>([])
     const [server, setServer] = useState<ServerInfoDto>()
 
     const getClientsList = async () => {
-        try {
-            const clients = await ClientService.getApiV1ServerClients()
-            setClients(clients)
-        }
-        catch (e) {
-            console.error(e)
-        }
+        setClients(await ClientService.getApiV1ServerClients())
     }
 
     const getServerInfo = async () => {
@@ -26,11 +21,22 @@ export const StatusPage = () => {
         setChannels(await ChannelService.getApiV1ServerChannels())
     }
 
-    useEffect(() => {
-        getClientsList()
+
+    if (!server) {
         getServerInfo()
         getChannelList()
-    }, [clients])
+        getClientsList()
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getServerInfo()
+            getChannelList()
+            getClientsList()
+        }, refreshInterval)
+
+        return () => clearInterval(interval);
+    }, [server, clients, channels])
 
     function renderClientsList(clients: ClientDto[]) {
         return (
@@ -38,26 +44,76 @@ export const StatusPage = () => {
         )
     }
 
-    function renderChannelList()
-    {
-        return(
+    function renderChannelList() {
+        return (
             <>
                 {channels.map(function (channel, index) {
-                    return(
+                    return (
                         <>
-                        <li className="border-solid py-2 border-b-2 rounded">
-                        <li key={index} className="ml-2 font-semibold"><FiCornerDownRight className="mr-1" />{channel.name}</li>
-                                <ul className="ml-2">
-                                    {renderClientsList(clients.filter(x => x.channelId === channel.id))}
+                            <li key={"header-" + { index }} className="border-solid py-2 border-b-2 rounded">
+                                <ul>
+                                    <li key={"channel-" + index} className="ml-2 font-semibold"><FiCornerDownRight className="mr-1" />{channel.name}</li>
+                                    <ul className="ml-2">
+                                        {renderClientsList(clients.filter(x => x.channelId === channel.id))}
+                                    </ul>
                                 </ul>
-                        </li>
+                            </li>
                         </>
                     )
                 })}
             </>
         )
     }
-    
+
+    const renderLoader = () => {
+        return (
+            <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+                        <stop stop-color="#fff" stop-opacity="0" offset="0%" />
+                        <stop stop-color="#fff" stop-opacity=".631" offset="63.146%" />
+                        <stop stop-color="#fff" offset="100%" />
+                    </linearGradient>
+                </defs>
+                <g fill="none" fill-rule="evenodd">
+                    <g transform="translate(1 1)">
+                        <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="2">
+                            <animateTransform
+                                attributeName="transform"
+                                type="rotate"
+                                from="0 18 18"
+                                to="360 18 18"
+                                dur="0.9s"
+                                repeatCount="indefinite" />
+                        </path>
+                        <circle fill="#fff" cx="36" cy="18" r="1">
+                            <animateTransform
+                                attributeName="transform"
+                                type="rotate"
+                                from="0 18 18"
+                                to="360 18 18"
+                                dur="0.9s"
+                                repeatCount="indefinite" />
+                        </circle>
+                    </g>
+                </g>
+            </svg>
+        )
+    }
+
+    const renderClients = () => {
+        return(
+            <div className="m-2 p-2 bg-gray-800 rounded-md w-3/4 text-left">
+            <ul className="m-4">
+                <li key="server-name" className="text-lg font-bold"><FiServer className="mr-1" />{server?.name}</li>
+                <ul className="ml-4">
+                    {renderChannelList()}
+                </ul>
+            </ul>
+        </div>
+                )
+    }
+
     return (
         <div>
             <div
@@ -66,14 +122,7 @@ export const StatusPage = () => {
                 <div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                     <div
                         className="w-full mt-3 md:w-1/4 bg-gray-800 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700">
-                        <div className="m-2 p-2 bg-gray-800 rounded-md w-3/4 text-left">
-                            <ul className="m-4">
-                                <li className="text-lg font-bold"><FiServer className="mr-1" />{server?.name} {server ? "Running for " + server.uptime : ""}</li>
-                                <ul className="ml-4">
-                                    {renderChannelList()}
-                                </ul>
-                            </ul>
-                        </div>
+                            {(server && channels) ? renderClients() : renderLoader()}
                     </div>
                 </div>
             </div>
