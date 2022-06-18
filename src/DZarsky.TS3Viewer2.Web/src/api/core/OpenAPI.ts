30612d9d7b07ed473e4ea17d60cbe062175b2c0c
Dominik Zarsky <dzarsky@dzarsky.eu>
2022-06-18 12:07:04 +0200

    add environments

--- src/DZarsky.TS3Viewer2.Web/src/api/core/OpenAPI.ts
+++ src/DZarsky.TS3Viewer2.Web/src/api/core/OpenAPI.ts
@@ -19,7 +19,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:5274',
+    BASE: process.env.API_BASE_URL as string,
     VERSION: '1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
