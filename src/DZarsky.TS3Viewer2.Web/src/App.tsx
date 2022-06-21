8c0f5764823320e201f31913ef84a6bc0563ca57
Dominik Zarsky <dzarsky@dzarsky.eu>
2022-06-21 20:49:28 +0200

    add footer, add empty server handling to main page

--- src/DZarsky.TS3Viewer2.Web/src/App.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/App.tsx
@@ -6,20 +6,37 @@ import { MainPage } from "./pages/Main"
 import { ConnectPage } from './pages/Connect';
 import { UploadPage } from './pages/Upload';
 import { AudioBotPage } from './pages/AudioBot';
+import { FiGithub } from 'react-icons/fi';
 
 
 function App() {
   return (
-      <div className="dark h-screen bg-gray-800">
-        <Navbar />
-          <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/status" element={<StatusPage />} />
-              <Route path="/connect" element={<ConnectPage />} />
-              <Route path="/upload" element={<UploadPage />} />
-              <Route path="/bot" element={<AudioBotPage />} />
-          </Routes>
-      </div>
+    <div className="dark h-screen bg-gray-800">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/status" element={<StatusPage />} />
+        <Route path="/connect" element={<ConnectPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/bot" element={<AudioBotPage />} />
+      </Routes>
+      <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 fixed inset-x-0 bottom-0">
+        <span className="text-sm text-gray-500 dark:text-gray-400 font-semibold">
+          Notice: Development still in progress, currently running the alpha/development version
+        </span>
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          © {new Date().getFullYear()} Dominik Zarsky
+        </span>
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          This project is GPL-3.0 licensed
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 mr-4 text-lg text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a href="https://github.com/Loupeznik/ts3viewer2" target="_blank" rel="noreferrer"><FiGithub /></a>
+          </li>
+        </ul>
+      </footer>
+    </div>
   );
 }
 
--- src/DZarsky.TS3Viewer2.Web/src/pages/Main.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/pages/Main.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {ServerInfoDto, ServerService} from "../api";
+import React, { useEffect, useState } from 'react';
+import { ServerInfoDto, ServerService } from "../api";
 
 export const MainPage = () => {
     const [server, setServer] = useState({})
@@ -19,12 +19,13 @@ export const MainPage = () => {
     }, [])
 
     function renderServerInfo(info: ServerInfoDto) {
-            return(
-                <div className="text-center">
-                    <div className="mb-1 text-md font-semibold">Currently connected users</div>
-                    <div className="my-2 font-sans text-xl font-bold">{info.clientsOnline}/{info.maxClients}</div>
-                </div>
-            )
+        let clientsOnline = (info.clientsOnline as number) - (info.queriesOnline as number)
+        return (
+            <div className="text-center">
+                <div className="mb-1 text-md font-semibold">Currently connected users</div>
+                <div className="my-2 font-sans text-xl font-bold">{!clientsOnline ? "0" : clientsOnline}/{!info.maxClients ? "0" : info.maxClients }</div>
+            </div>
+        )
     }
 
     return (
@@ -34,7 +35,7 @@ export const MainPage = () => {
                 <h1 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Current server status</h1>
                 <div className="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                     <div
-                       className="w-full mt-3 md:w-1/4 bg-gray-800 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700">
+                        className="w-full mt-3 md:w-1/4 bg-gray-800 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700">
                         {renderServerInfo(server)}
                     </div>
                 </div>
