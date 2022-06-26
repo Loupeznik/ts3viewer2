4fdd1a07542d1aff9af4c72e9fa9414a8c6198bc
Dominik Zarsky <dzarsky@dzarsky.eu>
2022-06-26 10:55:23 +0200

    remove footer

--- src/DZarsky.TS3Viewer2.Web/src/App.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/App.tsx
@@ -6,7 +6,6 @@ import { MainPage } from "./pages/Main"
 import { ConnectPage } from './pages/Connect';
 import { UploadPage } from './pages/Upload';
 import { AudioBotPage } from './pages/AudioBot';
-import { FiGithub } from 'react-icons/fi';
 
 
 function App() {
@@ -20,22 +19,6 @@ function App() {
         <Route path="/upload" element={<UploadPage />} />
         <Route path="/bot" element={<AudioBotPage />} />
       </Routes>
-      <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 fixed inset-x-0 bottom-0">
-        <span className="text-sm text-gray-500 dark:text-gray-400 font-semibold">
-          Notice: Development still in progress, currently running the alpha/development version
-        </span>
-        <span className="text-sm text-gray-500 dark:text-gray-400">
-          © {new Date().getFullYear()} Dominik Zarsky
-        </span>
-        <span className="text-sm text-gray-500 dark:text-gray-400">
-          This project is GPL-3.0 licensed
-        </span>
-        <ul className="flex flex-wrap items-center mt-3 mr-4 text-lg text-gray-500 dark:text-gray-400 sm:mt-0">
-          <li>
-            <a href="https://github.com/Loupeznik/ts3viewer2" target="_blank" rel="noreferrer"><FiGithub /></a>
-          </li>
-        </ul>
-      </footer>
     </div>
   );
 }
