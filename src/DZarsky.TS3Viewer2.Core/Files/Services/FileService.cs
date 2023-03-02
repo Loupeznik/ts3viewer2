6a05f78867f773da673fd18be2c86510a74cb226
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-03-02 19:25:54 +0100

    add uploaded filenames sanitization

--- src/DZarsky.TS3Viewer2.Core/Files/Services/FileService.cs
+++ src/DZarsky.TS3Viewer2.Core/Files/Services/FileService.cs
@@ -1,122 +1,135 @@
-﻿using DZarsky.TS3Viewer2.Domain.Files.Configuration;
+﻿using System.Globalization;
+using DZarsky.TS3Viewer2.Domain.Files.Configuration;
 using DZarsky.TS3Viewer2.Domain.Files.Constants;
 using DZarsky.TS3Viewer2.Domain.Files.Dto;
 using DZarsky.TS3Viewer2.Domain.Files.Services;
 using DZarsky.TS3Viewer2.Domain.Infrastructure.General;
 using HeyRed.Mime;
 using Serilog;
+using System.Text;
+using System.Text.RegularExpressions;
 
-namespace DZarsky.TS3Viewer2.Core.Files.Services
+namespace DZarsky.TS3Viewer2.Core.Files.Services;
+
+public sealed class FileService : IFileService
 {
-    public sealed class FileService : IFileService
+    private readonly FileConfig _fileConfig;
+    private readonly ILogger _logger;
+
+    public FileService(FileConfig fileConfig, ILogger logger)
     {
-        private readonly FileConfig _fileConfig;
-        private readonly ILogger _logger;
+        _fileConfig = fileConfig;
+        _logger = logger;
+    }
 
-        public FileService(FileConfig fileConfig, ILogger logger)
+    public async Task<ApiResult<AddFilesResultDto>> AddFiles(IDictionary<string, Stream> files)
+    {
+        var result = new AddFilesResultDto();
+
+        if (files.Count == 0)
         {
-            _fileConfig = fileConfig;
-            _logger = logger;
+            return ApiResult.Build(result, false, ReasonCodes.NullArgumentException, nameof(files));
         }
 
-        public async Task<ApiResult<AddFilesResultDto>> AddFiles(IDictionary<string, Stream> files)
+        CreateFilesDirectoryIfNotExists();
+
+        foreach (var file in files)
         {
-            var result = new AddFilesResultDto();
+            var fileName = ConvertFileName(file.Key);
+            var filePath = Path.Combine(_fileConfig.BasePath!, fileName);
 
-            if (files.Count == 0)
+            if (!FileConstants.AllowedMimeTypes.Contains(MimeGuesser.GuessMimeType(file.Value)) ||
+                File.Exists(filePath))
             {
-                return ApiResult.Build(result, false, ReasonCodes.NullArgumentException, nameof(files));
+                result.Failed.Add(file.Key);
+                continue;
             }
 
-            CreateFilesDirectoryIfNotExists();
-
-            foreach (var file in files)
+            await using (var stream = new FileStream(filePath, FileMode.Create))
             {
-                var filePath = Path.Combine(_fileConfig.BasePath!, file.Key);
-
-                if (!FileConstants.AllowedMimeTypes.Contains(MimeGuesser.GuessMimeType(file.Value)) || File.Exists(filePath))
-                {
-                    result.Failed.Add(file.Key);
-                    continue;
-                }
-
-                using (var stream = new FileStream(filePath, FileMode.Create))
-                {
-                    await file.Value.CopyToAsync(stream);
-                }
-
-                result.Successful.Add(file.Key);
+                await file.Value.CopyToAsync(stream);
             }
 
-            return ApiResult.Build(result);
+            result.Successful.Add(file.Key);
         }
 
-        public ApiResult<bool> DeleteFile(string? fullFileName)
+        return ApiResult.Build(result);
+    }
+
+    public ApiResult<bool> DeleteFile(string? fullFileName)
+    {
+        if (string.IsNullOrWhiteSpace(fullFileName) || !FilesDirectoryExists())
         {
-            if (string.IsNullOrWhiteSpace(fullFileName) || !FilesDirectoryExists())
-            {
-                return ApiResult.Build(false, false, ReasonCodes.InvalidArgument, nameof(fullFileName));
-            }
-
-            if (!FilesDirectoryExists())
-            {
-                return ApiResult.Build(false, false, ReasonCodes.NotFound);
-            }
-
-            var filePath = Path.Combine(_fileConfig.BasePath!, fullFileName);
-
-            if (!File.Exists(filePath))
-            {
-                return ApiResult.Build(false, false, ReasonCodes.NotFound);
-            }
-
-            try
-            {
-                File.Delete(filePath);
-            }
-            catch (Exception ex)
-            {
-                _logger.Error($"Could not delete file {filePath}: {ex}", ex);
-                return ApiResult.Build(false, false);
-            }
-
-            return ApiResult.Build(true);
+            return ApiResult.Build(false, false, ReasonCodes.InvalidArgument, nameof(fullFileName));
         }
 
-        public ApiResult<List<FileDto>> GetFiles()
+        if (!FilesDirectoryExists())
         {
-            var files = new List<FileDto>();
-
-            if (!FilesDirectoryExists())
-            {
-                return ApiResult.Build(files, false, ReasonCodes.NotFound);
-            }
-
-            var directoryFiles = Directory.GetFiles(_fileConfig.BasePath!);
-
-            foreach (var file in directoryFiles)
-            {
-                files.Add(new FileDto
-                {
-                    Path = file,
-                    FullName = Path.GetFileName(file),
-                    Name = Path.GetFileNameWithoutExtension(file)
-                });
-            }
-
-            return ApiResult.Build(files);
+            return ApiResult.Build(false, false, ReasonCodes.NotFound);
         }
 
-        private bool FilesDirectoryExists() => Directory.Exists(_fileConfig.BasePath);
+        var filePath = Path.Combine(_fileConfig.BasePath!, fullFileName);
 
-        private void CreateFilesDirectoryIfNotExists()
+        if (!File.Exists(filePath))
         {
-            if (FilesDirectoryExists() || string.IsNullOrWhiteSpace(_fileConfig.BasePath))
-            {
-                return;
-            }
-
-            Directory.CreateDirectory(_fileConfig.BasePath);
+            return ApiResult.Build(false, false, ReasonCodes.NotFound);
         }
+
+        try
+        {
+            File.Delete(filePath);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"Could not delete file {filePath}: {ex}", ex);
+            return ApiResult.Build(false, false);
+        }
+
+        return ApiResult.Build(true);
+    }
+
+    public ApiResult<List<FileDto>> GetFiles()
+    {
+        var files = new List<FileDto>();
+
+        if (!FilesDirectoryExists())
+        {
+            return ApiResult.Build(files, false, ReasonCodes.NotFound);
+        }
+
+        var directoryFiles = Directory.GetFiles(_fileConfig.BasePath!);
+
+        files.AddRange(directoryFiles.Select(file => new FileDto
+        {
+            Path = file,
+            FullName = Path.GetFileName(file),
+            Name = Path.GetFileNameWithoutExtension(file)
+        }));
+
+        return ApiResult.Build(files);
+    }
+
+    private bool FilesDirectoryExists() => Directory.Exists(_fileConfig.BasePath);
+
+    private void CreateFilesDirectoryIfNotExists()
+    {
+        if (FilesDirectoryExists() || string.IsNullOrWhiteSpace(_fileConfig.BasePath))
+        {
+            return;
+        }
+
+        Directory.CreateDirectory(_fileConfig.BasePath);
+    }
+
+    private static string ConvertFileName(string fileName)
+    {
+        var regex = new Regex("[^a-zA-Z0-9\\.]+");
+
+        var normalizedString = fileName.Normalize(NormalizationForm.FormD);
+        var asciiString = new string(normalizedString
+            .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+            .ToArray());
+
+        return regex.Replace(asciiString, "_").ToLower();
     }
 }
