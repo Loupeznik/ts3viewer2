6b2546446986dd562b388a59ee8753866328adf6
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-03-04 15:10:13 +0100

    add sentry support to webapp

--- src/DZarsky.TS3Viewer2.Web/src/index.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/index.tsx
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import 'flowbite';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN as string,
+  integrations: [new BrowserTracing()],
+  release: import.meta.env.VITE_VERSION as string,
+
+  tracesSampleRate: 1.0,
+  environment: import.meta.env.VITE_SENTRY_ENVIRONMENT as string,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
