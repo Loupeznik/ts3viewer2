6a68158edc5f8340fe653422656f18dc0b3eb3fd
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-03-04 11:37:42 +0100

    add pwa

--- src/DZarsky.TS3Viewer2.Web/vite.config.ts
+++ src/DZarsky.TS3Viewer2.Web/vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(), 
+    viteTsconfigPaths(), 
+    svgrPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      }
+    })
+  ],
   build: {
     outDir: 'build',
   },
