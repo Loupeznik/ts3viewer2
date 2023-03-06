3816b9f9e4879c74bf9e68abe80516cde34852d5
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-03-06 16:43:58 +0100

    add option to disable sentry tracing

--- src/DZarsky.TS3Viewer2.Web/src/index.tsx
+++ src/DZarsky.TS3Viewer2.Web/src/index.tsx
@@ -11,7 +11,7 @@ Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN as string,
   integrations: [new BrowserTracing()],
   release: import.meta.env.VITE_VERSION as string,
-  enabled: false,
+  enabled: import.meta.env.VITE_SENTRY_ENABLED as boolean,
 
   tracesSampleRate: 1.0,
   environment: import.meta.env.VITE_SENTRY_ENVIRONMENT as string,
